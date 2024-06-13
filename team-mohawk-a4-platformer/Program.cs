fb4098bb28a97f5f2030d720caaa6f878e818cd1
Muhammad Asad <000880751@mohawkcollege.ca>
2024-06-13 11:36:37 -0400

    Add different sound effect

--- team-mohawk-a4-platformer/Program.cs
+++ team-mohawk-a4-platformer/Program.cs
@@ -1,16 +1,13 @@
 ﻿using Raylib_cs;
 using System.Numerics;
-using System.Runtime.CompilerServices;
 
 public class Program
 {
     // If you need variables in the Program class (outside functions), you must mark them as static
-    static string title = "Flappin Birb"; // Window title
+    static string title = "Flappy Bird"; // Window title
     static int screenWidth = 800; // Screen width
     static int screenHeight = 600; // Screen height
     static int targetFps = 60; // Target frames-per-second
-    static bool isMouseButtonPressed = Raylib.IsMouseButtonPressed(MouseButton.Left);
-    static bool isKeyPressed = Raylib.IsKeyPressed(KeyboardKey.Space);
 
 
 
@@ -18,6 +15,11 @@ public class Program
 
     static void Main()
     {
+        // Create a window to draw to. The arguments define width and height
+        Raylib.InitWindow(screenWidth, screenHeight, title);
+        // Set the target frames-per-second (FPS)
+        Raylib.SetTargetFPS(targetFps);
+        //
 
         // Enable audio
         Raylib.InitAudioDevice();
@@ -26,23 +28,40 @@ public class Program
         // Loop so long as window should not close
         while (!Raylib.WindowShouldClose())
         {
-
-
-            // Stop audio
-            Raylib.CloseAudioDevice();
-            // Close the window
-            Raylib.CloseWindow();
+            // Enable drawing to the canvas (window)
+            Raylib.BeginDrawing();
+            // Clear the canvas with one color
+            Raylib.ClearBackground(Color.RayWhite);
+            // Your game code here. This is a function YOU define.
+            Update();
+            // Stop drawing to the canvas, begin displaying the frame
+            Raylib.EndDrawing();
         }
+        // Close the window
+        Raylib.CloseWindow();
+    }
 
-        static void Setup()
-        {
+    static void Setup()
+    {
+        // Your one-time setup code here
 
+        // load sound here
+        sfx = Raylib.LoadSound("../../../SMACK Sound Effect.mp3"); 
+        
 
-            LoadSFX();
-        }
+    }
 
         static void Update()
         {
+    static void Update()
+    {
+        // Your game code run each frame here
+
+        // play sound here
+        if (Raylib.IsKeyPressed(KeyboardKey.Space))
+        {
+            Raylib.PlaySound(sfx);
+        }
 
 
             // Your game code run each frame here
