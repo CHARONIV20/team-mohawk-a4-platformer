6c85dc83b620a2bf18ada6a534d2fdf16b95184e
Stephan <wearshark13@gmail.com>
2024-06-13 11:11:01 -0400

    add picture files to classes

--- team-mohawk-a4-platformer/Assets.cs
+++ team-mohawk-a4-platformer/Assets.cs
@@ -1,10 +1,22 @@
-﻿using System;
+﻿using Raylib_cs;
+using System;
 
 public class pipeAssets
 {
-	public pipeAssets()
-	{
+    public static Texture2D texture;
+    public static void load()
+    {
+        //load texture
+        Image image = Raylib.LoadImage("../../../Assets-Graphics/pipe.png");
+        texture = Raylib.LoadTextureFromImage(image);
 
+        Raylib.UnloadImage(image);
 
-	}
+    }
+    public static void Draw()
+    {
+        Raylib.DrawTexture(texture, 0, 0, Color.White);
+
+    }
+
 }
--- team-mohawk-a4-platformer/Background.cs
+++ team-mohawk-a4-platformer/Background.cs
@@ -1,11 +1,25 @@
-﻿using static Raylib_cs.Raylib;
+﻿using Raylib_cs;
 
 namespace team_mohawk_a4_platformer
 {
-    internal class Background
+    
+    public class Background
     {
+        public static Texture2D texture;
+        public static void load()
+        {
 
+            //load texture
+            Image image = Raylib.LoadImage("../../../Assets-Graphics/bg_5.png");
+            texture = Raylib.LoadTextureFromImage(image);
 
+            Raylib.UnloadImage(image);
+        }
+        public static void Draw()
+        {
+            Raylib.DrawTexture(texture,0,0,Color.White);
+
+        }
 
     }
 }
